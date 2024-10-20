571fad8a729241b5dc907bdfc4904eb5cd339da6
Yeeloman <yami.onlyme@gmail.com>
2024-10-20 14:42:19 +0100

    modulating the handtracking class

--- main.py
+++ main.py
@@ -17,11 +17,9 @@ def main():
             print("Failed to capture image")
             break
         
-        # Resize the image to improve processing speed
         img = cv2.flip(img, cv2.ROTATE_180)
         img = cv2.resize(img, (frame_width, frame_height))
 
-        # Find hands and positions
         img = detector.findHands(img, draw=False)
         lmList = detector.findPosition(img, draw=False)
 
@@ -32,12 +30,10 @@ def main():
         fps = 1 / (cTime - pTime)
         pTime = cTime
 
-        # Display FPS on the image (optional)
         cv2.putText(img, f'FPS: {int(fps)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
 
         cv2.imshow("Image", img)
 
-        # Introduce a slight delay for frame display
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
